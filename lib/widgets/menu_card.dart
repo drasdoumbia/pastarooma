5610037785fba7eb8e06a44e2a79bf98e9d65e33
Dramane DOUMBIA <ddramane63@gmail.com>
2020-08-07 14:01:13 +0000

    added menuCard widget

---
+++ lib/widgets/menu_card.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import 'outlineButton.dart';
+
+class MenuCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 245.0,
+      decoration: BoxDecoration(
+        color: kSecondaryColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFF5F8D29).withOpacity(0.2),
+            spreadRadius: 5,
+            blurRadius: 30,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Stack(
+        children: <Widget>[
+          ClipPath(
+            clipper: ClippingClass(),
+            child: Container(
+              width: 372.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  topLeft: Radius.circular(20.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Our menu", style: Theme.of(context).textTheme.headline1),
+                    Text("Check out the delivery menu", style: Theme.of(context).textTheme.bodyText1),
+                    SizedBox(height: 15.0),
+                    OutlineBtn(
+                      buttonTitle: "Check",
+                      onTap: () => {
+                        // Do something
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -10.0,
+            bottom: -50.0,
+            width: 186.0,
+            height: 186.0,
+            child: Image.asset('assets/images/menu_img_1.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ClippingClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 110);
+    path.quadraticBezierTo(size.width / 4, size.height - 40, size.width / 2, size.height - 40);
+    path.quadraticBezierTo(size.width - (size.width / 4), size.height - 40, size.width, size.height - 110);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldCLipper) => false;
+}
