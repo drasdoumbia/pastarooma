be302476489e7fa2cc4c0257911979d15480831d
Dramane DOUMBIA <ddramane63@gmail.com>
2020-08-07 12:05:17 +0000

    added new color

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 // project colors
 const kPrimaryColor = Color(0xFFFFA726);
 const kSecondaryColor = Color(0xFFFF4D26);
+const kOrangeLight = Color(0xFFFFA726);
 const kGreyDark = Color(0xFF363E36);
 const kGreyLight = Color(0xFFA5A5A5);
